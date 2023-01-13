a25ff858a6c1dcacb031d88c2e692dbfc5d65eae
max-grzanna <grzannamax@gmail.com>
2023-01-13 10:32:36 +0100

    add header img

--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -1,18 +1,16 @@
 import React from "react";
-import { Typography } from "antd";
+import { Typography, Image } from "antd";
 
 const { Title, Text } = Typography;
 
 // displays a page header
 
-export default function Header({ link, title, subTitle, ...props }) {
+export default function Header({ link, title, subTitle, imgSrc, ...props }) {
   return (
     <div style={{ display: "flex", justifyContent: "space-between", padding: "1.2rem" }}>
       <div style={{ display: "flex", flexDirection: "column", flex: 1, alignItems: "start" }}>
         <a href={link} target="_blank" rel="noopener noreferrer">
-          <Title level={4} style={{ margin: "0 0.5rem 0 0" }}>
-            {title}
-          </Title>
+          <Image src={imgSrc} />
         </a>
         <Text type="secondary" style={{ textAlign: "left" }}>
           {subTitle}
@@ -26,4 +24,5 @@ export default function Header({ link, title, subTitle, ...props }) {
 Header.defaultProps = {
   link: "https://github.com/scaffold-eth/scaffold-eth",
   title: "Stadt Leipzig",
+  imgSrc: "https://static.leipzig.de/typo3conf/ext/mkleipzig/Resources/Public/img/logo.png",
 };
